bd31914267f59706e1a6074a37dcbe027db65dfe
Alexandre <souzajr.alexandre@gmail.com>
2024-05-07 17:49:54 -0300

    Criação dos projetos microsserviços

---
+++ compras-api/src/main/java/com/ecommerce/compras/api/ComprasApiApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.compras.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComprasApiApplication.class, args);
+	}
+
+}
